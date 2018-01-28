7198564be457cfeb3af0ff0ba83d218fa7c251e9
Sini Liu <jia.2.liu@aalto.fi>
2018-01-28 20:15:43 +0200

    lisää tehtävä 1.5

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -3,36 +3,41 @@ import ReactDOM from 'react-dom';
 
 
 const App = () => {
-	const kurssi = 'Half Stack -sovelluskehitys'
+	const kurssi = {
+		nimi: 'Half Stack -sovelluskehitys',
+		osat: [
+			{
+				nimi: 'Reactin perusteet',
+				tehtavia: 10
+			},
 
-	const osat = [
-		{
-			nimi: 'Reactin perusteet',
-			tehtavia: 10
-		},
+	
+			{
+				nimi: 'Tiedonvälitys propseilla',
+				tehtavia: 7
+			},
+	
+			{
+				nimi: 'Komponenttien tila',
+				tehtavia: 14
+			}
+		]
+	}
 
+	function yhteensa(a, b, c) {
+		return a + b + c
+	}
 	
-		{
-			nimi: 'Tiedonvälitys propseilla',
-			tehtavia: 7
-		},
-	
-		{
-			nimi: 'Komponenttien tila',
-			tehtavia: 14
-		}
-	]
-	
+	const vastaus = yhteensa(kurssi.osat[0].tehtavia, kurssi.osat[1].tehtavia, kurssi.osat[2].tehtavia)
 
 	return (
 		<div>
-			<h1>{kurssi}</h1>
-			<p>{osat[0].nimi} {osat[0].tehtavia}</p>
-			<p>{osat[1].nimi} {osat[1].tehtavia}</p>
-			<p>{osat[2].nimi} {osat[2].tehtavia}</p>
-			<p>yhteensä 31 {osat.forEach((lkm) => {
-				console.log(osat[0].tehtavia + osat[1].tehtavia + osat[2].tehtavia)
-			})} tehtävää</p>
+			<h1>{kurssi.nimi}</h1>
+			<p>{kurssi.osat[0].nimi} {kurssi.osat[0].tehtavia}</p>
+			<p>{kurssi.osat[1].nimi} {kurssi.osat[1].tehtavia}</p>
+			<p>{kurssi.osat[2].nimi} {kurssi.osat[2].tehtavia}</p>
+			
+			<p>yhteensä {vastaus} tehtävää</p>
 		</div>
 	)
 }
